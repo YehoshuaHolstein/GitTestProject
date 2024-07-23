9dfe393649305fa2914ea94a2f3c08f68a925b54
Yehoshua Holstein <holsteinyehoshua@gmail.com>
2024-07-23 11:54:25 -0400

    Added comments about branches

--- src/com/coderscampus/Profile.java
+++ src/com/coderscampus/Profile.java
@@ -2,8 +2,29 @@ package com.coderscampus;
 
 import java.time.LocalDate;
 
+/*
+ * 
+ * Branches
+ * main/master -> represents the code that's actually live on the internet{i.e. coderscampus.com)
+ * UAT (not typical)
+ * QA
+ * Development
+ * 
+ * 
+ * Environments
+ * Production - main/master
+ * UAT (not typical)
+ * QA
+ * Dev
+ * 
+ * new stuff!!
+ * 
+ */
+
 public class Profile {
 	private Integer id;
 	private String name;
 	private LocalDate createddate;
+	
+	
 }
